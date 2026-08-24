8a42155672ee711d6fb5af0c09f5191bdea9135e
Manuel Ruiz Cerezo <manuel.ruiz@idom.com>
2026-08-24 10:19:56 +0200

    revert: quitar detalle de error de BBDD en respuesta publica

--- server/index.js
+++ server/index.js
@@ -32,8 +32,7 @@ const dbReady = initDb();
 app.use((req, res, next) => {
   dbReady.then(() => next(), (err) => {
     console.error('[db] Error inicializando la base de datos:', err.message);
-    // TODO temporal: quitar el detalle del error una vez diagnosticado el fallo en Vercel
-    res.status(503).json({ error: 'Base de datos no disponible', detail: err.message });
+    res.status(503).json({ error: 'Base de datos no disponible' });
   });
 });
 
